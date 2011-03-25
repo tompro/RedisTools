40589a2f42bb7d23db9bebf7953c9feca613e329
protom <office@protom.eu>
2011-03-25 22:30:39 +0100

    Finished first version of simple key store

--- lib/PRTools/Key.php
+++ lib/PRTools/Key.php
@@ -4,6 +4,7 @@
  * 
  * @author Thomas Profelt <office@protom.eu>
  * @since 24.03.2011
+ * @version 1.0
  */
 namespace PRTools;
 
@@ -11,39 +12,97 @@ class Key extends Core\Dataconstruct
 {
 	
 	/**
-	 * @var mixed - can contain basic datatypes (string, int, ...)
+	 * set a simple string $value into this Key
+	 * the optional $ttl lets this Key expire after
+	 * $ttl seconds
+	 * 
+	 * @param string $value
+	 * @param int $ttl - the time to live in seconds
+	 * @return boolean - success of operation
 	 */
-	protected $value = false;
-
-	
-	public function set( $value )
+	public function set( $value, $ttl = null )
 	{
-		if($this->getRedis()->set( $this->getKey(), $value ))
+		if( $ttl === null )
 		{
-			$this->value = $value;
-			return true;
+			return $this->getRedis()->set( 
+				$this->getKey(), 
+				$value 
+			);
 		}
 		
-		return false;
+		return $this->getRedis()->setex( 
+			$this->getKey(), 
+			$ttl, 
+			$value 
+		);
 	}
 	
+	/**
+	 * returns the string value stored in a key
+	 * 
+	 * @return string | boolean false if key does not exists
+	 */
 	public function get()
 	{
-		if( ! $this->value )
-		{
-			$this->value = $this->getRedis()->get( $this->getKey() );
-		}
-		return $this->value;
+		return $this->getRedis()->get( $this->getKey() );
 	}
 	
+	/**
+	 * deletes this key and value
+	 * 
+	 * @return boolean - success
+	 */
 	public function delete()
 	{
-		$result = $this->getRedis()->delete( $this->getKey() );
-		if($result === 1)
-		{
-			$this->value = false;
-		}
-		return $result;
+		return $this->getRedis()->delete( $this->getKey() );
+	}
+	
+	/**
+	 * returns the remaining time to live of this key in 
+	 * seconds. Returns -1 if key has no expire date
+	 * 
+	 * @return int - the remaining time to live in seconds 
+	 */
+	public function ttl()
+	{
+		return $this->getRedis()->ttl( $this->getKey() );
+	}
+	
+	/**
+	 * determine wether this key already exists
+	 * @return boolean
+	 */
+	public function exists()
+	{
+		return $this->getRedis()->exists( $this->getKey() );
+	}
+	
+	/**
+	 * sets a new $value to the key if the key does not exist
+	 * 
+	 * @param string $value
+	 * @return boolean - success 
+	 */
+	public function setIfNotExists( $value )
+	{
+		return $this->getRedis()->setnx( 
+			$this->getKey(), $value );
+	}
+	
+	/**
+	 * set an expiration date for this key.
+	 * Accepts a unix timestamp. Returns false if key
+	 * does not exist.
+	 * 
+	 * @param int $timestamp
+	 * @return boolean - success 
+	 */
+	public function expireAt( $timestamp )
+	{
+		return $this->getRedis()->expireAt( 
+			$this->getKey(), 
+			$timestamp 
+		);
 	}
 	
 }
--- test/lib/PRTools/KeyTest.php
+++ test/lib/PRTools/KeyTest.php
@@ -18,6 +18,7 @@ class KeyTest extends \PHPUnit_Framework_TestCase
 
 	protected $testValue = 'testvalue';
 
+	protected $testTtl = 2;
 
 	/**
 	 * Sets up the fixture, for example, opens a network connection.
@@ -51,6 +52,121 @@ class KeyTest extends \PHPUnit_Framework_TestCase
 		);
 	}
 	
+	public function testSetValueWithExpires()
+	{
+		$this->assertTrue(
+			$this->object->set( 
+				$this->testValue,
+				$this->testTtl
+			),
+			'Value with expires time could not be written to Redis. '
+		);
+		
+		$ttl = $this->object->ttl();
+		$this->assertEquals(
+			$this->testTtl, $ttl,
+			'TTl should be '. $this->testTtl . ' but was ' . $ttl . '. '
+		);
+	}
+	
+	public function testIfKeyExistsOnEmptyKey()
+	{
+		$this->assertFalse(
+			$this->object->exists(),
+			'Key should not exist but did not return false. '
+		);
+	}
+
+
+	public function testGettingTtlOnEmptyValue()
+	{
+		$this->assertEquals(
+			-1, $this->object->ttl(),
+			'Ttl of empty key should return -1 but was not. '
+		);
+	}
+	
+	public function testGettingTtlOfNotExpiringValue()
+	{
+		$this->object->set($this->testValue);
+		
+		$this->assertEquals(
+			-1, $this->object->ttl(),
+			'Ttl of non expiring key should return -1 but was not. '
+		);
+	}
+
+	public function testSetNonExistingKey()
+	{
+		$this->assertTrue(
+			$this->object->setIfNotExists( $this->testValue ),
+			'Setting value on empty key was not successful. '
+		);
+		
+		$this->assertEquals(
+			$this->testValue, 
+			$this->object->get(),
+			'Value should have been set but was not. '
+		);
+	}
+
+	public function testSetIfNotExistsWithExistingValue()
+	{
+		$this->object->set($this->testValue);
+		
+		$this->assertFalse(
+			$this->object->setIfNotExists( 'some other' ),
+			'Setting value should not be successful but was. '
+		);
+		
+		$this->assertEquals(
+			$this->testValue, 
+			$this->object->get(),
+			'Value has been changed but should not. '
+		);
+	}
+
+	public function testSettingExpireValue()
+	{
+		$this->object->set($this->testValue);
+		
+		$offset = 2;
+		
+		$this->assertTrue(
+			$this->object->expireAt( \time() + $offset ),
+			'Setting expire date was not successful. '
+		);
+		
+		$ttl = $this->object->ttl();
+		$this->assertEquals(
+			$offset,
+			$ttl,
+			'TTL should be ' . $offset . ' but was ' . $ttl . '. '
+		);
+	}
+	
+	public function testSettingExpireValueInThePast()
+	{
+		$this->object->set($this->testValue);
+		$this->assertTrue(
+			$this->object->expireAt( 1 ),
+			'Setting expire value in the past was not posible. '
+		);
+		
+		$this->assertFalse(
+			$this->object->exists(),
+			'Setting ttl value in the past should expire key immediately but did not. '
+		);
+	}
+	
+	public function testSettingExpireValueOnEmptyKey()
+	{
+		$this->assertFalse(
+			$this->object->expireAt( \time() + 2 ),
+			'Setting expire date should not be possible on empty keys. '
+		);
+	}
+
 	public function testSettingValueWithRedisError()
 	{
 		$redis = $this->getMock('Redis', array('set'));
